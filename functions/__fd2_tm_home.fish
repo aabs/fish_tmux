1818d27e8c7929f57b7b3a9da78c684eaa075c55
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-22 10:28:14 +1000

    initial

--- functions/__fd2_tm_home.fish
+++ functions/__fd2_tm_home.fish
@@ -3,10 +3,10 @@ function __fd2_tm_home -d "goto home dir of current tmuxer"
   echo "vars: $NUM_TMUX_ENV_VARS"
   if test $NUM_TMUX_ENV_VARS -eq 0
     info "attaching"
-    tmux -2 attach -t $CURRENT_TMUX_SESSION
+    tmux -2 attach -t $fd2_current_tmux_session
   else
     info "switching"
-    tmux switch -t $CURRENT_TMUX_SESSION
+    tmux switch -t $fd2_current_tmux_session
   end
 end
 
