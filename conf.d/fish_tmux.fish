665538a9a20a0b15650bc5e8e1c335db6dbf41f7
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-31 09:32:17 +1000

    updated to new subcmd std

--- conf.d/fish_tmux.fish
+++ conf.d/fish_tmux.fish
@@ -21,13 +21,13 @@ if not set -q $fd2_current_tmux_session
   set -U fd2_current_tmux_session "$fish_projects_current_sn"
 end
 
-fd2_define_command tm "fishdots plugin for using tmux"
+fd2_define_command -p tm -d "fishdots plugin for using tmux"
 
-fd2_define_subcommand tm set __fd2_tm_set "set current TMUX session"
-fd2_define_subcommand tm new __fd2_tm_new "<session_name> create a new TMUX session"
-fd2_define_subcommand tm detach __fd2_tm_detach "disconnect from the current session"
-fd2_define_subcommand tm goto __fd2_tm_goto "<name> change tmux sessions"
-fd2_define_subcommand tm home __fd2_tm_home "got to the current home tmux session"
-fd2_define_subcommand tm ls __fd2_tm_list "list all available tmuxers"
-fd2_define_subcommand tm open __fd2_tm_open "open from list dialog"
-fd2_define_subcommand tm session __fd2_tm_session "display the current session name"
+fd2_define_subcommand -p tm -c set -f __fd2_tm_set -d "set current TMUX session"
+fd2_define_subcommand -p tm -c new -f __fd2_tm_new -d "<session_name> create a new TMUX session"
+fd2_define_subcommand -p tm -c detach -f __fd2_tm_detach -d "disconnect from the current session"
+fd2_define_subcommand -p tm -c goto -f __fd2_tm_goto -d "<name> change tmux sessions"
+fd2_define_subcommand -p tm -c home -f __fd2_tm_home -d "got to the current home tmux session"
+fd2_define_subcommand -p tm -c ls -f __fd2_tm_list -d "list all available tmuxers"
+fd2_define_subcommand -p tm -c open -f __fd2_tm_open -d "open from list dialog"
+fd2_define_subcommand -p tm -c session -f __fd2_tm_session -d "display the current session name"
