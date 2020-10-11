6d8e6a7b30fbe3a7b9b4d2bdf10a87799fb90492
Andrew Matthews <matthews.andrew@gmail.com>
2020-10-12 10:53:53 +1100

    wip

--- conf.d/fish_tmux.fish
+++ conf.d/fish_tmux.fish
@@ -10,7 +10,7 @@
 abbr --add tl   'tm ls'
 
 # tn <name>: create a session named <name>
-abbr --add tn   'tm new'
+abbr --add tn   'tm new -n '
 
 # ta <name>: attach to a session named <name>
 abbr --add ta   'tmux -2 attach -t'
